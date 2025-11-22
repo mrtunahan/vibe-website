7c572b6f75721b713edb6ef7cf9274efe9cc25e3
mrtunahan <tunahankorkmaz6@gmail.com>
2025-11-22 18:27:25 +0300

    Vibe basliyor

---
+++ script.js
@@ -0,0 +1,7 @@
+const button = document.getElementById('vibeBtn');
+
+button.addEventListener('click', () => {
+    alert("Vibe Check Başarılı! Kodlama modu devrede. 🚀");
+    button.innerText = "Keyifler Yerinde 😎";
+    button.style.backgroundColor = "#50fa7b";
+});
