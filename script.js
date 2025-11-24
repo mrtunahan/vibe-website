d95720f52e4764b25aa74d804fe978d8a3e82e39
mrtunahan <tunahankorkmaz6@gmail.com>
2025-11-25 00:55:17 +0300

    Sistem tamamen yenilendi: Ajan, Kopya Korumasi ve Numara Kontrolu

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 // ==================================================================
 // ⚠️ DİKKAT: BURADAKİ URL SİZİN KENDİ APPSCRIPT URL'NİZ OLMALI
 // ==================================================================
-const GOOGLE_SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbxSi-ZwZ2YdY_-qrHaJZDk_KbqkgKXHz1J6Si_JIffftMLlTwjeghq_vXQi0zxVkD1F/exec';
+const GOOGLE_SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbx7kwbKhfGSQpkud9EB6je-KwCKsyxujHSu-yvXPE3dfdiXX8odvaexBNUCBHbDjKB2/exec';
 
 // Global değişkenler
 let questionsSource = [];
